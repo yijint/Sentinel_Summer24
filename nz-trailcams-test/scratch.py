c7e1ff413f0a6a34433937a0ac391a8eddcae38c
Sam Kelly <samkelly5941@gmail.com>
2024-06-21 12:48:08 -0400

    debugging delete statement

--- nz-trailcams-test/scratch.py
+++ nz-trailcams-test/scratch.py
@@ -11,18 +11,16 @@ cloud_sql_connection_name = 'sentinel-project-278421:us-east4:training-data'
 TABLE_NAME = 'images_v3'
 DATASET_NAME = 'nz-trailcams-test'
 URL = f'mysql+pymysql://{db_user}:{db_pass}@{db_ip}/{db_name}'
+engine = sqlalchemy.create_engine(URL, pool_size=5, max_overflow=2, pool_timeout=30, pool_recycle=1800)
 
 # read dataset
 def print_dataset():
-    engine = sqlalchemy.create_engine(URL, pool_size=5, max_overflow=2, pool_timeout=30, pool_recycle=1800)
     query = f'SELECT * FROM images.{TABLE_NAME}'
     query += f' WHERE dataset = "{DATASET_NAME}"'
     db_df = pd.read_sql(query, con=engine)
     return db_df
-
 # delete data
 def delete_dataset():
-    engine = sqlalchemy.create_engine(URL, pool_size=5, max_overflow=2, pool_timeout=30, pool_recycle=1800)
     with engine.connect() as connection:
         query = f'DELETE FROM images.{TABLE_NAME}'
         query += f' WHERE dataset = "{DATASET_NAME}"'
@@ -30,7 +28,7 @@ def delete_dataset():
 
 # test
 with open("scratch.txt", 'w') as f:
-    print(f"dataset:\n{print_dataset()}", file = f, flush=True)
+    print(print_dataset(), file = f, flush=True)
     print("==========================================================", file = f, flush=True)
     try:
         delete_dataset()
@@ -38,5 +36,4 @@ with open("scratch.txt", 'w') as f:
     except Exception as ex:
         print(f"Deletion failed: {ex}", file = f, flush=True)
     print("==========================================================", file = f, flush=True)
-    print(f"dataset:\n{print_dataset()}", file = f, flush=True)
-9
+    print(print_dataset(), file = f, flush=True)
